d383345dd168ee2144a0034dc282c65fa59d0e32
Vitalic <0makovskyi@gmail.com>
2024-12-12 14:34:10 +0200

    added route and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import "./App.css";
+import { lazy } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Layout } from './components/layout';
+import { RestrictedRoute } from './components/RestrictedRoute/RestrictedRoute';
+import './App.css';
 
 function App() {
+  const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+  const WelcomePage = lazy(() => import('./pages/WelcomePage/WelcomePage'));
+  const SigninPage = lazy(() => import('./pages/SigninPage/SigninPage'));
+  const SignupPage = lazy(() => import('./pages/SignupPage/SignupPage'));
+  const NotFoundPage = lazy(() => import('./pages/NotFoundPage'));
+
   return (
-    <>
-      <h1>Hello Team</h1>
-    </>
+    <Layout>
+      <Routes>
+        <Route path='/welcome' element={<WelcomePage />} />
+        <Route path='/signin' element={<SigninPage />} />
+        <Route path='/signup' element={<SignupPage />} />
+        <Route
+          path='/home'
+          element={
+            <RestrictedRoute component={<HomePage />} redirectTo='/welcome' />
+          }
+        />
+        <Route path='/' element={<Navigate to='/welcome' replace />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
