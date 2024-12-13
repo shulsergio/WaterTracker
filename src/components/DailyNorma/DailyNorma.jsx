b4d2efbcafbd92a8eba07c7b8e03c82480ed360e
OstapUrdey <43811378+OstapUrdey@users.noreply.github.com>
2024-12-13 14:44:48 +0200

    fix

--- src/components/DailyNorma/DailyNorma.jsx
+++ src/components/DailyNorma/DailyNorma.jsx
@@ -1,36 +1,42 @@
-// import { useState } from 'react';
-// import styles from './DailyNorma.module.css';
+import { useState } from 'react';
+import styles from './DailyNorma.module.css';
+import MyDailyNormaModal from '../MyDailyNormaModal/MyDailyNormaModal.jsx';
+import Button from '../button/Button.jsx';
 
-// const DailyNorma = () => {
-//     const [dailyNorm, setDailyNorm] = useState(1.5);
-//     const [isModalOpen, setIsModalOpen] = useState(false);
+const DailyNorma = () => {
+    const [dailyNorm, setDailyNorm] = useState(1.5);
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
-//     const handleEditClick = () => {
-//         setIsModalOpen(true);
-//     };
+    const handleEditClick = () => {
+        setIsModalOpen(true);
+    };
 
-//     const handleModalClose = (newNorm) => {
-//         if (newNorm) setDailyNorm(newNorm);
-//         setIsModalOpen(false);
-//     };
+    const handleModalClose = (newNorm) => {
+        if (newNorm) setDailyNorm(newNorm);
+        setIsModalOpen(false);
+    };
 
-//     return (
-//         <div className={styles.container}>
-//             <h2 className={styles.title}>My daily norma</h2>
-//             <div className={styles.value}>
-//                 <span className={styles.norma}>{dailyNorm} L</span>
-//                 <button className={styles.editButton} onClick={handleEditClick}>
-//                     Edit
-//                 </button>
-//             </div>
-//             {isModalOpen && (
-//                 <DailyNormaModal
-//                     initialNorm={dailyNorm}
-//                     onClose={handleModalClose}
-//                 />
-//             )}
-//         </div>
-//     );
-// };
+    return (
+        <div className={styles.container}>
+            <h2 className={styles.title}>My daily norma</h2>
+            <div className={styles.value}>
+                <span className={styles.norma}>{dailyNorm} L</span>
+                <Button
+                    className={styles.editButton}
+                    type="text"
+                    onClick={handleEditClick}
+                >
+                    Edit
+                </Button>
+            </div>
+            {isModalOpen && (
+                <MyDailyNormaModal
+                    initialNorm={dailyNorm}
+                    onClose={handleModalClose}
+                />
+            )}
+        </div>
+    );
+};
 
-// export default DailyNorma;
+export default DailyNorma;
