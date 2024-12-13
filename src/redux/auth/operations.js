e2378b62d474d51412e4a0a168e63e7164a13fa6
Alona Zabolotskykh <alenka.polishchuk@gmail.com>
2024-12-13 13:35:26 +0200

    signup-redux-part1

---
+++ src/redux/auth/operations.js
@@ -0,0 +1,21 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+axios.defaults.baseURL = "https://bo-o-woa.onrender.com/";
+
+const setAuthHeader = (token) => {
+  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+};
+
+export const signUp = createAsyncThunk(
+  "auth/signup",
+  async (credentials, thunkApi) => {
+    try {
+      const { data } = await axios.post("/user/signup", credentials);
+      setAuthHeader(data.token);
+      //   console.log(data);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
