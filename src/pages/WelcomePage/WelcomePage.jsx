fe2ed9a7293a550533aa90e7ce7504602bbd65d7
Yuliia Vorobei <vorobeju1@gmail.com>
2024-12-14 19:47:49 +0100

    added changes

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -4,19 +4,15 @@ import css from "./WelcomePage.module.css";
 export default function WelcomePage() {
   console.log("Rendered WelcomePage");
   return (
-    <ul className={css.list}>
+    <section className={css.section}>
       <div className={css.wrapper}>
-        <div className={css.container}>
-          <li>
-            <WaterСonsumptionTracker />
-          </li>
+        <div className={css.containerWater}>
+          <WaterСonsumptionTracker />
         </div>
-        <div>
-          <li>
-            <WhyDrinkWater />
-          </li>
+        <div className={css.containerWhy}>
+          <WhyDrinkWater />
         </div>
       </div>
-    </ul>
+    </section>
   );
 }
