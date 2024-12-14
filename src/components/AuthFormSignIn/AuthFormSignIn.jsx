909394e8a740f954437ac54448427b091ad7e0f9
Sergey Shulga <shul.sergio@gmail.com>
2024-12-14 21:34:21 +0200

    add signIn

--- src/components/AuthFormSignIn/AuthFormSignIn.jsx
+++ src/components/AuthFormSignIn/AuthFormSignIn.jsx
@@ -3,66 +3,102 @@ import css from "./AuthFormSignIn.module.css";
 import Button from "../button/Button";
 import { NavLink, useNavigate } from "react-router-dom";
 // import { signIn } from "../../redux/auth/operations";
-import { logIn  } from "../../redux/auth/operations";
+import { logIn } from "../../redux/auth/operations";
 import { useDispatch } from "react-redux";
 import * as Yup from "yup";
-
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useState } from "react";
 
 const validationSchema = Yup.object({
-  email: Yup.string().email("Invalid email address").required("Email is required"),
-  password: Yup.string().min(8, "Password must be at least 8 characters").required("Password is required"),
+  email: Yup.string()
+    .email("Invalid email address")
+    .required("Email is required"),
+  password: Yup.string()
+    .min(8, "Password must be at least 8 characters")
+    .required("Password is required"),
 });
 
 export default function AuthFormSignIn() {
-  
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleSubmit = (values, actions) => {
-    dispatch(logIn (values));
-    navigate("/home");
+    setIsLoading(true);
+    console.log("LoginForm values: ", values);
+    console.log(
+      "Current Authorization Header ONE:",
+      axios.defaults.headers.common.Authorization
+    );
 
-    actions.resetForm();
+    dispatch(logIn(values))
+      .unwrap()
+      .then(() => {
+        toast.success("OK! You are logged");
+        console.log(
+          "Current Authorization Header FINISH:",
+          axios.defaults.headers.common.Authorization
+        );
+        navigate("/home");
+      })
+      .catch(() => {
+        toast.error("Error, mistake!");
+      })
+      .finally(() => {
+        setIsLoading(false);
+        actions.resetForm();
+      });
   };
-    
-    return (
-      <div className={css.container}>
-    <Formik
-      initialValues={{
-        email: "",
-        password: "",
-      }}
-          validationSchema={validationSchema}
-          onSubmit={handleSubmit}
-            >
-            <Form className={css.form}>
-            <p className={css.text}>Sign In</p>
-            <label className={css.label}>
+
+  return (
+    <div className={css.container}>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        <Form className={css.form}>
+          <p className={css.text}>Sign In</p>
+          <label className={css.label}>
             Enter your email
-            <Field type="email" name="email" className={css.input} placeholder="E-mail" />
-            </label>
-            <ErrorMessage
-          name="email"
-          component="div"
-          className={css.messageError}
-        />
-         <label className={css.label}>
-        Enter your password
-        <Field type="password" name="password" className={css.input} placeholder="Password" />
-            </label>
-            <ErrorMessage
-          name="password"
-          component="div"
-          className={css.messageError}
-        />
-        <Button className={css.button}>Sign In</Button>
+            <Field
+              type="email"
+              name="email"
+              className={css.input}
+              placeholder="E-mail"
+            />
+          </label>
+          <ErrorMessage
+            name="email"
+            component="div"
+            className={css.messageError}
+          />
+          <label className={css.label}>
+            Enter your password
+            <Field
+              type="password"
+              name="password"
+              className={css.input}
+              placeholder="Password"
+            />
+          </label>
+          <ErrorMessage
+            name="password"
+            component="div"
+            className={css.messageError}
+          />
+          <Button type="submit" className={css.button}>
+            Sign In
+          </Button>
         </Form>
       </Formik>
       <nav className={css.nav}>
         <NavLink to="/signup">Sign Up</NavLink>
-        </nav>
-      </div>
-);
+      </nav>
+    </div>
+  );
 }
-
-
