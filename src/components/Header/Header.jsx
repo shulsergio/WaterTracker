92e9c3bc57bfef0b2a33d7443585388c6a89ca11
Sergey Shulga <shul.sergio@gmail.com>
2024-12-14 17:40:52 +0200

    del HeaderContainer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,20 @@
-import { useSelector } from 'react-redux';
-import css from './Header.module.css';
-import UserAuth from '../UserAuth/UserAuth';
-import UserLogo from '../UserLogo/UserLogo';
-import { selectIsLoggedIn } from '../../redux/auth/selectors';
-import Logo from '../Logo/Logo';
-
+import { useSelector } from "react-redux";
+import css from "./Header.module.css";
+import UserAuth from "../UserAuth/UserAuth";
+import UserLogo from "../UserLogo/UserLogo";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
+import Logo from "../Logo/Logo";
 
 const Header = () => {
-    const isLoggedIn = useSelector(selectIsLoggedIn);
-    
-    return (
-      <div className={css.headerContainer}>
-        <header className={css.header}>
-          <Logo />
-          {isLoggedIn ? <UserLogo /> : <UserAuth />}
-        </header>
-      </div>
-    );
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return (
+    // <div className={css.headerContainer}>
+    <header className={css.header}>
+      <Logo />
+      {isLoggedIn ? <UserLogo /> : <UserAuth />}
+    </header>
+    // </div>
+  );
 };
 export default Header;
