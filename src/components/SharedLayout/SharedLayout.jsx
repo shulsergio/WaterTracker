1734094702ddde7ac841844705e93f8a38d33688
Sergey Shulga <shul.sergio@gmail.com>
2024-12-14 15:16:01 +0200

    fix SharedLayout

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,15 +1,18 @@
 import { Suspense } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
+import styles from "./SharedLayout.module.css";
 
 const SharedLayout = () => {
   return (
-    <>
-      <Header />
-      <Suspense fallback={null}>
-        <Outlet />
-      </Suspense>
-    </>
+    <div className={styles.background}>
+      <div className={styles.container}>
+        <Header />
+        <Suspense fallback={null}>
+          <Outlet />
+        </Suspense>
+      </div>
+    </div>
   );
 };
 export default SharedLayout;
