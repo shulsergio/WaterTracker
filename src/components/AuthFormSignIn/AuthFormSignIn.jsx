0cccdcd8ab3eee5630e3aed3f4cd52d7ce5e39f4
Inna Starovytska <starovitckayainna1@gmail.com>
2024-12-15 10:38:51 +0200

    AuthFormSignIn+css

--- src/components/AuthFormSignIn/AuthFormSignIn.jsx
+++ src/components/AuthFormSignIn/AuthFormSignIn.jsx
@@ -6,6 +6,9 @@ import { NavLink, useNavigate } from "react-router-dom";
 import { logIn  } from "../../redux/auth/operations";
 import { useDispatch } from "react-redux";
 import * as Yup from "yup";
+import { useState } from "react";
+import { BiHide, BiShow } from "react-icons/bi";
+
 
 
 const validationSchema = Yup.object({
@@ -18,13 +21,20 @@ export default function AuthFormSignIn() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleSubmit = (values, actions) => {
-    dispatch(logIn (values));
-    navigate("/home");
+  const [showPassword, setShowPassword] = useState(false);
 
+  const togglePasswordVisibility = () => setShowPassword((prev) => !prev);
+  
+  const handleSubmit = (values, actions) => {
+    try {
+      dispatch(logIn(values));
+      navigate("/home");
+    } catch (error) {
+      alert (`Sign In failed:${ErrorMessage}` )
+    }
     actions.resetForm();
   };
-    
+  
     return (
       <div className={css.container}>
     <Formik
@@ -34,35 +44,62 @@ export default function AuthFormSignIn() {
       }}
           validationSchema={validationSchema}
           onSubmit={handleSubmit}
-            >
+        >
+          {({ errors, touched }) => (
             <Form className={css.form}>
-            <p className={css.text}>Sign In</p>
-            <label className={css.label}>
-            Enter your email
-            <Field type="email" name="email" className={css.input} placeholder="E-mail" />
-            </label>
-            <ErrorMessage
-          name="email"
-          component="div"
-          className={css.messageError}
-        />
-         <label className={css.label}>
-        Enter your password
-        <Field type="password" name="password" className={css.input} placeholder="Password" />
-            </label>
-            <ErrorMessage
-          name="password"
-          component="div"
-          className={css.messageError}
-        />
-        <Button className={css.button}>Sign In</Button>
-        </Form>
-      </Formik>
-      <nav className={css.nav}>
-        <NavLink to="/signup">Sign Up</NavLink>
-        </nav>
-      </div>
-);
+              <p className={css.text}>Sign In</p>
+
+              <label className={css.label}>
+                Enter your email
+                <Field type="email" name="email" className={`${css.input} ${touched.email && errors.email ? css.inputError : ""
+                  }`} placeholder="E-mail" />
+              </label>
+              <ErrorMessage
+                name="email"
+                component="div"
+                className={css.errorMessage}
+              />
+            
+              <label className={css.label}>
+                Enter your password
+                <div className={css.passwordWrapper}>
+                  <Field type={showPassword ? "text" : "password"} name="password" className={
+                    touched.password && errors.password
+                      ? `${css.input} ${css.inputError}`
+                      : css.input
+                  }
+                    placeholder="Password" />
+                  <button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className={css.eyeButton}
+                    aria-label="Toggle password visibility"
+                  >
+                    {showPassword ? (
+                      <BiShow className={css.eye} />
+                    ) : (
+                      <BiHide className={css.eye} />
+                    )}
+                  </button>
+                </div>
+              </label>
+              <ErrorMessage
+                name="password"
+                component="div"
+                className={css.errorMessage}
+              />
+              <Button type="submit" className={css.signinButton}>Sign In</Button>
+            </Form>
+          )}
+            </Formik>
+      <nav className={css.navWrap}>
+        <NavLink className={css.nav} to="/signup">
+          Sign Up
+        </NavLink>
+      </nav>
+    </div>
+  );
 }
+
 
 
