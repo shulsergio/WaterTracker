c97ab3be64e2179406b792b64689872a694fcacb
Inna Starovytska <starovitckayainna1@gmail.com>
2024-12-16 22:16:57 +0200

    SigninPage

--- src/pages/SigninPage/SigninPage.jsx
+++ src/pages/SigninPage/SigninPage.jsx
@@ -1,10 +1,16 @@
 import AuthFormSignIn from "../../components/AuthFormSignIn/AuthFormSignIn";
+import css from "./SigninPage.module.css";
 
 export default function SigninPage() {
   console.log("Rendered SigninPage");
   return (
     <>
-      <AuthFormSignIn />
+      <div className={css.container}>
+        <div className={css.background}>
+            <AuthFormSignIn />
+           <div className={css.backgroundImage}> </div>
+        </div>
+        </div>
     </>
   );
 }
