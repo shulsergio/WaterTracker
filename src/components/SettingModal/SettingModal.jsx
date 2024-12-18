be8c159bbb2f1eb5fefbeac8e69c01b61fb78185
Sergey Shulga <shul.sergio@gmail.com>
2024-12-18 22:14:43 +0200

    fix setting modal

--- src/components/SettingModal/SettingModal.jsx
+++ src/components/SettingModal/SettingModal.jsx
@@ -10,8 +10,9 @@ import styles from "./SettingModal.module.css"; // Стилі форми
 import { selectUser } from "../../redux/user/selectors.js";
 import RadioButton from "../radio-button/RadioButton.jsx";
 import Button from "../button/Button.jsx";
+import Modal from "../Modal/Modal.jsx";
 
-const SettingModal = ({ onSave }) => {
+const SettingModal = ({ onClose }) => {
   const dispatch = useDispatch();
   const avatarUrl = useSelector((state) => state.user.avatarUrl);
   const email = useSelector(selectUser).email;
@@ -83,154 +84,155 @@ const SettingModal = ({ onSave }) => {
   };
 
   return (
-    <div>
-      <div style={{ textAlign: "center" }}>
-        <label htmlFor="fileInput" style={{ cursor: "pointer" }}>
-          <img
-            src={preview || avatarUrl || "default-avatar.png"}
-            alt="Avatar"
-            style={{
-              width: "150px",
-              height: "150px",
-              borderRadius: "50%",
-              objectFit: "cover",
-              marginBottom: "10px",
-            }}
+    <Modal title="Setting" onClose={onClose}>
+      <>
+        <div style={{ textAlign: "center" }}>
+          <label htmlFor="fileInput" style={{ cursor: "pointer" }}>
+            <img
+              src={preview || avatarUrl || "default-avatar.png"}
+              alt="Avatar"
+              style={{
+                width: "150px",
+                height: "150px",
+                borderRadius: "50%",
+                objectFit: "cover",
+                marginBottom: "10px",
+              }}
+            />
+            <p style={{ color: "#007BFF" }}>Upload a photo</p>
+          </label>
+          <input
+            id="fileInput"
+            type="file"
+            accept="image/*"
+            style={{ display: "none" }}
+            onChange={handleFileChange}
           />
-          <p style={{ color: "#007BFF" }}>Upload a photo</p>
-        </label>
-        <input
-          id="fileInput"
-          type="file"
-          accept="image/*"
-          style={{ display: "none" }}
-          onChange={handleFileChange}
-        />
-      </div>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        {({ values, setFieldValue }) => (
-          <Form className={styles.form}>
-            <div className={styles.fieldGroup}>
-              <label className={styles.label}>Your gender identity</label>
-              <RadioButton
-                value="Women"
-                selectedValue={values.gender}
-                onChange={() => setFieldValue("gender", "Women")}
-                label="Women"
-              />
-              <RadioButton
-                value="Men"
-                selectedValue={values.gender}
-                onChange={() => setFieldValue("gender", "Men")}
-                label="Men"
-              />
-            </div>
+        </div>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleSubmit}
+        >
+          {({ values, setFieldValue }) => (
+            <Form className={styles.form}>
+              <div className={styles.fieldGroup}>
+                <label className={styles.label}>Your gender identity</label>
+                <RadioButton
+                  value="Women"
+                  selectedValue={values.gender}
+                  onChange={() => setFieldValue("gender", "Women")}
+                  label="Women"
+                />
+                <RadioButton
+                  value="Men"
+                  selectedValue={values.gender}
+                  onChange={() => setFieldValue("gender", "Men")}
+                  label="Men"
+                />
+              </div>
 
-            <div className={styles.fieldGroup}>
-              <label htmlFor="name" className={styles.label}>
-                Your name
-              </label>
-              <Field
-                id="name"
-                name="name"
-                type="text"
-                placeholder="Enter your name (optional)"
-                className={styles.input}
-              />
-            </div>
+              <div className={styles.fieldGroup}>
+                <label htmlFor="name" className={styles.label}>
+                  Your name
+                </label>
+                <Field
+                  id="name"
+                  name="name"
+                  type="text"
+                  placeholder="Enter your name (optional)"
+                  className={styles.input}
+                />
+              </div>
 
-            <div className={styles.fieldGroup}>
-              <label htmlFor="email" className={styles.label}>
-                E-mail
-              </label>
-              <Field
-                id="email"
-                name="email"
-                type="email"
-                className={styles.input}
-              />
-              <ErrorMessage
-                name="email"
-                component="div"
-                className={styles.error}
-              />
-            </div>
+              <div className={styles.fieldGroup}>
+                <label htmlFor="email" className={styles.label}>
+                  E-mail
+                </label>
+                <Field
+                  id="email"
+                  name="email"
+                  type="email"
+                  className={styles.input}
+                />
+                <ErrorMessage
+                  name="email"
+                  component="div"
+                  className={styles.error}
+                />
+              </div>
 
-            <div className={styles.fieldGroup}>
-              <label className={styles.label} style={{ fontWeight: "bold" }}>
-                Password
-              </label>
-              <div>
-                <div className={styles.fieldGroup}>
-                  <label htmlFor="outdatedPassword" className={styles.label}>
-                    Outdated password
-                  </label>
-                  <Field
-                    id="outdatedPassword"
-                    name="outdatedPassword"
-                    type="password"
-                    placeholder="Enter your current password"
-                    className={styles.input}
-                  />
-                  <ErrorMessage
-                    name="outdatedPassword"
-                    component="div"
-                    className={styles.error}
-                  />
-                </div>
+              <div className={styles.fieldGroup}>
+                <label className={styles.label} style={{ fontWeight: "bold" }}>
+                  Password
+                </label>
+                <div>
+                  <div className={styles.fieldGroup}>
+                    <label htmlFor="outdatedPassword" className={styles.label}>
+                      Outdated password
+                    </label>
+                    <Field
+                      id="outdatedPassword"
+                      name="outdatedPassword"
+                      type="password"
+                      placeholder="Enter your current password"
+                      className={styles.input}
+                    />
+                    <ErrorMessage
+                      name="outdatedPassword"
+                      component="div"
+                      className={styles.error}
+                    />
+                  </div>
 
-                <div className={styles.fieldGroup}>
-                  <label htmlFor="newPassword" className={styles.label}>
-                    New password
-                  </label>
-                  <Field
-                    id="newPassword"
-                    name="newPassword"
-                    type="password"
-                    placeholder="Enter your new password"
-                    className={styles.input}
-                  />
-                  <ErrorMessage
-                    name="newPassword"
-                    component="div"
-                    className={styles.error}
-                  />
-                </div>
+                  <div className={styles.fieldGroup}>
+                    <label htmlFor="newPassword" className={styles.label}>
+                      New password
+                    </label>
+                    <Field
+                      id="newPassword"
+                      name="newPassword"
+                      type="password"
+                      placeholder="Enter your new password"
+                      className={styles.input}
+                    />
+                    <ErrorMessage
+                      name="newPassword"
+                      component="div"
+                      className={styles.error}
+                    />
+                  </div>
 
-                <div className={styles.fieldGroup}>
-                  <label htmlFor="repeatNewPassword" className={styles.label}>
-                    Repeat new password
-                  </label>
-                  <Field
-                    id="repeatNewPassword"
-                    name="repeatNewPassword"
-                    type="password"
-                    placeholder="Repeat your new password"
-                    className={styles.input}
-                  />
-                  <ErrorMessage
-                    name="repeatNewPassword"
-                    component="div"
-                    className={styles.error}
-                  />
+                  <div className={styles.fieldGroup}>
+                    <label htmlFor="repeatNewPassword" className={styles.label}>
+                      Repeat new password
+                    </label>
+                    <Field
+                      id="repeatNewPassword"
+                      name="repeatNewPassword"
+                      type="password"
+                      placeholder="Repeat your new password"
+                      className={styles.input}
+                    />
+                    <ErrorMessage
+                      name="repeatNewPassword"
+                      component="div"
+                      className={styles.error}
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
 
-            <Button types="primary" type="submit" className={styles.saveButton}>
-              Save
-            </Button>
-          </Form>
-        )}
-      </Formik>
-    </div>
+              <Button type="primary" className={styles.saveButton}>
+                Save
+              </Button>
+            </Form>
+          )}
+        </Formik>
+      </>
+    </Modal>
   );
 };
-
 export default SettingModal;
 
 // const handleSave = () => {
