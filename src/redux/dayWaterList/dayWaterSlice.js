cdf381ea4613fbdf770b84ce4b40282e2d43366d
Yuliia Vorobei <vorobeju1@gmail.com>
2024-12-20 23:26:24 +0100

    added delete option

--- src/redux/dayWaterList/dayWaterSlice.js
+++ src/redux/dayWaterList/dayWaterSlice.js
@@ -39,16 +39,20 @@ const dayWaterSlice = createSlice({
       .addCase(updateWaterGlass.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
-        const index = state.data.findIndex((log) => log.id === action.payload);
+        const index = state.data.logs.findIndex(
+          (log) => log.id === action.payload
+        );
         if (index !== -1) {
-          state.data[index] = action.payload;
+          state.data.logs[index] = action.payload;
         }
       })
 
       .addCase(deleteWaterGlass.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
-        state.data = state.data.filter((log) => log.id !== action.payload.id);
+        state.data.logs = state.data.logs.filter(
+          (log) => log.id !== action.payload
+        );
       })
 
       .addCase(deleteWaterGlass.rejected, (state, action) => {
@@ -62,24 +66,24 @@ const dayWaterSlice = createSlice({
           getDayWaterList.pending,
           addWaterGlass.pending,
           updateWaterGlass.pending,
-          deleteWaterGlass.pending,
+          deleteWaterGlass.pending
         ),
         (state) => {
           state.isLoading = true;
           state.error = null;
-        },
+        }
       )
 
       .addMatcher(
         isAnyOf(
           getDayWaterList.rejected,
           addWaterGlass.rejected,
-          updateWaterGlass.rejected,
+          updateWaterGlass.rejected
         ),
         (state, action) => {
           state.isLoading = false;
           state.error = action.payload;
-        },
+        }
       );
   },
 });
