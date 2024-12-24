163a71600a7064cb6893e392caa25d3b8e1b152c
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-12-24 14:08:52 +0200

    CSS

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -4,6 +4,35 @@ import Header from "../Header/Header";
 import styles from "./SharedLayout.module.css";
 import { useSelector } from "react-redux";
 import Loader from "../Loader/Loader";
+import BackgroundSVG from "../../components/BackgroundSVG/backgroundSVG.jsx";
+      
+
+const SharedLayout = () => {
+  const isLoading = useSelector((state) => state.auth.isLoading);
+  return (
+    <>
+      {isLoading && <Loader />}
+{/*       <div > */}
+{/*         <BackgroundSVG /> */}
+{/*         <div className={styles.container}> */}
+          <Header />
+          <Suspense fallback={null}>
+            <Outlet />
+          </Suspense>
+{/*         </div> */}
+{/*       </div> */}
+      
+    </>
+  );
+};
+export default SharedLayout;
+
+/* import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../Header/Header";
+import styles from "./SharedLayout.module.css";
+import { useSelector } from "react-redux";
+import Loader from "../Loader/Loader";
 
 const SharedLayout = () => {
   const isLoading = useSelector((state) => state.auth.isLoading);
@@ -25,3 +54,4 @@ const SharedLayout = () => {
   );
 };
 export default SharedLayout;
+ */
