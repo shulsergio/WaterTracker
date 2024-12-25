19425ed5067e3268fef7790863e21d43e8251353
Іван <65630800+bahioiiia@users.noreply.github.com>
2024-12-25 04:03:16 +0200

    welcome

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,26 +1,22 @@
 import WhyDrinkWater from "../../components/WhyDrinkWater/WhyDrinkWater";
 import WaterСonsumptionTracker from "../../components/WaterСonsumptionTracker/WaterСonsumptionTracker";
 import css from "./WelcomePage.module.css";
-import BackgroundSVG from "../../components/BackgroundSVG/backgroundSVG.jsx";
 
 export default function WelcomePage() {
   console.log("Rendered WelcomePage");
   return (
-    <div>
-{/*       <BackgroundSVG /> */}
-      <div className={css.background}>
-        <div className={css.layer}>
-          <section className={css.section}>
-            <div className={css.wrapper}>
-              <div className={css.containerWater}>
-                <WaterСonsumptionTracker />
-              </div>
-              <div className={css.containerWhy}>
-                <WhyDrinkWater />
-              </div>
+    <div className={css.background}>
+      <div className={css.layer}>
+        <section className={css.section}>
+          <div className={css.wrapper}>
+            <div className={css.containerWater}>
+              <WaterСonsumptionTracker />
             </div>
-          </section>
-        </div>
+            <div className={css.containerWhy}>
+              <WhyDrinkWater />
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
